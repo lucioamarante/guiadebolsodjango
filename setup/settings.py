548bc1c2f26bf52180aab752ce00c422154b4fb9
lucioamarante <luciosk@gmail.com>
2026-03-11 21:34:40 -0300

    arquivos do projeto dois

--- setup/settings.py
+++ setup/settings.py
@@ -126,4 +126,4 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
